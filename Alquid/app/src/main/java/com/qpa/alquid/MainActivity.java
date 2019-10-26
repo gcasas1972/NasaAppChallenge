fa57196690eaf3b76dce824abf01e63e517593f6
SkyDrax09 <skydrax09@gmail.com>
2019-10-26 00:01:11 -0300

    Updated App with Runtime Permissions Requests
    - Added Runtime Permissions  Request when opening App

--- Alquid/app/src/main/java/com/qpa/alquid/MainActivity.java
+++ Alquid/app/src/main/java/com/qpa/alquid/MainActivity.java
@@ -1,21 +1,38 @@
 package com.qpa.alquid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.karan.churi.PermissionManager.PermissionManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    PermissionManager permissionManager;
+    TextView txtDenied,txtGranted;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtGranted=(TextView)findViewById(R.id.txtGranted);
+        txtDenied=(TextView)findViewById(R.id.txtDenied);
+        permissionManager = new PermissionManager() {
+        };
+        permissionManager.checkAndRequestPermissions(this);
+
 
         BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
         androidx.navigation.ui.AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -30,7 +47,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       //             .replace(R.id.container, new CameraFragment())
       //             .commit();
       // }
+
+
+
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionManager.checkResult(requestCode, permissions, grantResults);
+
+        ArrayList<String> granted = permissionManager.getStatus().get(0).granted;
+        ArrayList<String> denied = permissionManager.getStatus().get(0).denied;
+
+        for (String item:granted)
+            txtGranted.setText(txtGranted.getText()+"\n"+item);
+        for (String item:denied)
+            txtDenied.setText(txtDenied.getText()+"\n"+item);
+    }
+
 
     @Override
     public void onClick(View v) {
