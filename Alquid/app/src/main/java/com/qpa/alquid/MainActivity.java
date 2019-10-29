962e49ff34d98e5d5a08a875a0ec541d79448786
Damián Ponce <dami.ponce@hotmail.com>
2019-10-29 01:08:12 -0300

    Transparent notification bar.

--- Alquid/app/src/main/java/com/qpa/alquid/MainActivity.java
+++ Alquid/app/src/main/java/com/qpa/alquid/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
+import android.graphics.Color;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Fullscreen
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+     // requestWindowFeature(Window.FEATURE_NO_TITLE);
+     // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 R.id.navigation_home, R.id.navigation_camera, R.id.navigation_weather, R.id.navigation_user)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
       // if (savedInstanceState == null) {
@@ -55,10 +55,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       //             .replace(R.id.container, new CameraFragment())
       //             .commit();
       // }
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS, true);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
+   // <item name="windowNoTitle">true</item>
+   //     <item name="windowActionBar">false</item>
+   //     <item name="android:windowTranslucentStatus">false</item>
+   //     <item name="android:windowTranslucentNavigation">false</item>
+//
+   //     <item name="android:windowDrawsSystemBarBackgrounds">true</item>
+   //     <item name="android:statusBarColor">@android:color/transparent</item>
 
+    }
 
-
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     @Override
